67b81494eace9dffa65a7a915f35735b8ac2ec6b
Eliav Maman <eliav@myinpact.com>
2015-07-28 01:23:36 +0300

    added functionality

--- decription/config/env/all.js
+++ decription/config/env/all.js
@@ -18,6 +18,7 @@ module.exports = {
 			],
 			js: [
 				'public/lib/angular/angular.js',
+				'public/lib/jquery/dist/jquery.js',
 				'public/lib/angular-resource/angular-resource.js', 
 				'public/lib/angular-cookies/angular-cookies.js', 
 				'public/lib/angular-animate/angular-animate.js', 
@@ -25,7 +26,9 @@ module.exports = {
 				'public/lib/angular-sanitize/angular-sanitize.js', 
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
 				'public/lib/angular-ui-utils/ui-utils.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+				'public/lib/ng-s3upload/build/ng-s3upload.js',
+				'public/lib/moment/moment.js'
 			]
 		},
 		css: [
---
+++ decription/public/modules/users/controllers/myDecriptions.client.controller.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('users').controller('MyDecriptionController', ['$scope', '$http', '$location', 'Authentication', '$timeout',
+  function ($scope, $http, $location, Authentication, $timeout) {
+    $scope.authentication = Authentication;
+
+    // If user is signed in then redirect back home
+    //   if ($scope.authentication.user) $location.path('/');
+
+    $scope.init = function () {
+      $scope.getUserDecription();
+    };
+    $scope.getUserDecription = function () {
+      $http.get('/decriptions', {params: {email: $scope.authentication.user.email}}).success(function (response) {
+
+        $scope.decriptions = response;
+      }).error(function (response) {
+        $scope.error = response.message;
+      });
+    };
+  }
+]);
