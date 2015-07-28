7c997424a5b75acb6d8335f4c990c2d5d67f23c1
Eliav Maman <eliav@myinpact.com>
2015-07-28 17:22:48 +0300

    added grunt to heroku

--- decription/gruntfile.js
+++ decription/gruntfile.js
@@ -167,9 +167,9 @@ module.exports = function(grunt) {
 
 	// Heroku task(s).
 	grunt.registerTask('heroku', ['build']);
-	grunt.registerTask('heroku:integration', ['build']);
-	grunt.registerTask('heroku:production', ['build']);
-	grunt.registerTask('heroku:development', ['build']);
+	//grunt.registerTask('heroku:integration', ['build']);
+	//grunt.registerTask('heroku:production', ['build']);
+	//grunt.registerTask('heroku:development', ['build']);
 	// Test task.
 	grunt.registerTask('test', ['env:test', 'mochaTest', 'karma:unit']);
 
